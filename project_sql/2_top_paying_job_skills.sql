c044b8a7e13dbfbf35e1997c4fd35d58438f33ad
Tim Yu <gallonlabs@gmail.com>
2025-10-27 18:06:11 +0700

    upload sql files

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,50 @@
+
+WITH top_paying_jobs AS (
+SELECT
+    job_id,
+    job_title,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst'  AND job_location = 'Anywhere'
+    AND salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
+)
+SELECT 
+    top_paying_jobs.*,
+    skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY salary_year_avg DESC
+
+
+
+WITH remote_job_skills AS (
+    SELECT
+        skill_id,
+        COUNT(*) AS skill_count
+    FROM
+        skills_job_dim AS skills_to_job
+    INNER JOIN job_postings_fact AS job_postings ON job_postings.job_id = skills_to_job.job_id
+    WHERE
+        job_postings.job_work_from_home = TRUE AND
+        job_postings.job_title_short = 'Data Analyst'
+    GROUP BY
+        skill_id)
+
+SELECT
+    skills.skill_id,
+    skills AS skill_name,
+    skill_count
+FROM remote_job_skills
+INNER JOIN skills_dim AS skills ON skills.skill_id = remote_job_skills.skill_id
+ORDER BY
+    skill_count DESC
+LIMIT 5;
